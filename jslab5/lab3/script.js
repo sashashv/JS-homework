90fe62ddae6ab7da2a8043e8699326be46ada4e4
sashashv <sashash29@mail.ru>
2017-06-16 12:05:06 +0300

    search elements in js/Fitness/

---
+++ jslab5/lab3/script.js
@@ -0,0 +1,12 @@
+"use strict";
+var elemLogo = document.querySelector('div > a > img');
+console.log(elemLogo);
+
+var  leftMenuText = document.querySelector('.left_menu').textContent;
+console.log(leftMenuText);
+
+var bikeImg = document.querySelector('.bike img');
+console.log(bikeImg);
+
+var copyrigthText = document.querySelector('.footer p').textContent;
+console.log(copyrigthText);
